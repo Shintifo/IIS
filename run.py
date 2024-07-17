989c37f878c8c4ad284e4c4177f6b24b03888396
t <t.brayko@innopolis.university>
2024-07-17 15:35:52 +0300

    Update

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 import faiss
+from tqdm import tqdm
 
 
 def extract_features(images_dir):
@@ -21,9 +22,12 @@ def extract_features(images_dir):
 	])
 
 	imgs = [t(img).unsqueeze(0).numpy() for img in imgs]
-
 	session = onnxruntime.InferenceSession(f"WrapperONNX.onnx")
-	out = [session.run(None, {"l_tensor_": img})[0] for img in imgs]
+
+	out = []
+	for img in tqdm(imgs):
+		out.append(session.run(None, {"l_tensor_": img})[0])
+	# out = [session.run(None, {"l_tensor_": img})[0] for img in imgs]
 	img_feats = torch.tensor(np.vstack(out))
 	X = F.normalize(img_feats, p=2, dim=1)
 	return X
@@ -36,6 +40,24 @@ def qimg_embedding(qimg, X, images_dir):
 	return Q
 
 
+def count_qimg(path_qimg):
+	image = Image.open(path_qimg)
+
+	t = transforms.Compose([
+		transforms.Resize((500, 500)),
+		transforms.ToTensor(),
+		transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+	])
+
+	q = t(image).unsqueeze(0).numpy()
+
+	session = onnxruntime.InferenceSession(f"WrapperONNX.onnx")
+	q = session.run(None, {"l_tensor_": q})[0]
+	q = torch.tensor(np.vstack(q))
+	q = F.normalize(q, p=2, dim=1)
+	return q
+
+
 def save_index(X):
 	index = faiss.IndexFlatL2(2048)
 	index.add(X)
@@ -46,20 +68,24 @@ def fais(X, query_embedding):
 	save_index(X)
 	index = faiss.read_index("index_file.index")
 
-	k = X.shape[0]
+	k = min(100, X.shape[0])
 
 	distances, indices = index.search(query_embedding, k)
+
+	indices = indices[0]
+	distances = distances[0]
 	indices = indices[np.where(distances <= 0.25)]
 	print(f"Indices of nearest neighbors: {indices}")
-	print(distances)
+	for i in indices:
+		print(distances[i])
 
 
-def main(dataset_name):
+def main(dataset_name, qimg):
 	images_dir = f"{os.getcwd()}/datasets/{dataset_name}"
 	s = time.time()
 	X = extract_features(images_dir)
 	print(time.time() - s)
-	Q = qimg_embedding("A1.jpg", X, images_dir)
+	Q = qimg_embedding(qimg, X, images_dir)
 	fais(X, Q)
 
 
@@ -67,4 +93,5 @@ if __name__ == "__main__":
 	# parser = argparse.ArgumentParser(description='Find Similar Images')
 	# parser.add_argument('qimg', type=str, help='Query Image')
 	# args = parser.parse_args()
-	main('custom')
+	# fais(None, count_qimg("datasets/roxford5k/all_souls_000002.jpg"))
+	main('roxford5k', "all_souls_000002.jpg")
